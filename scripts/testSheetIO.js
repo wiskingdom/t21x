458feea0dece241ad53b1b1aa6d0ca3b96bc940e
Yeonwoo Kim <wiskingdom@gmail.com>
2020-12-31 21:47:48 +0900

    Edit IO api

--- scripts/testSheetIO.js
+++ scripts/testSheetIO.js
@@ -3,14 +3,13 @@
 const path = require('path');
 // const fs = require('fs');
 // custom modules
-const { readCommonSheet, writeCommonSheet } = require('../lib/sheetIO');
+const { readSheet, writeSheet } = require('../lib/sheetIO');
 
 const inputPath = process.argv[2];
 const { name, ext } = path.parse(inputPath);
 
-console.log('reading...');
-const { data } = readCommonSheet(inputPath, 0);
+const { data } = readSheet(0, inputPath);
 
 console.log(data[14243]);
 
-writeCommonSheet(path.join('./output', `${name}_out${ext}`), 'data', { data });
+writeSheet(path.join('./output', `${name}_out${ext}`), 'data', { data });
